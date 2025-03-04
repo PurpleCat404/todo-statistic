511de825c979c23e9a038d51c72e745fa3819015
flexihtala <flexihtala@gmail.com>
2025-03-04 16:35:29 +0500

    5.1 5.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
 
@@ -11,43 +11,70 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
-function getAllTasks(){
+function getAllTasks() {
     let tasks = [];
     for (const file of files) {
         for (const line of file.split('\n')) {
-         if (line.trim().startsWith('// TODO'))
-            tasks.push(line.trim());
+            if (line.trim().startsWith('// TODO'))
+                tasks.push(line.trim());
         }
     }
-    
+
     return tasks;
 }
 
 function processCommand(command) {
-    let tasks;
+    let tasks = getAllTasks();
     switch (true) {
         case command === 'exit':
             process.exit(0);
             break;
         case command === 'show':
-            tasks = getAllTasks()
             for (const task of tasks)
                 console.log(task);
             break;
         case command === 'important':
-            tasks = getAllTasks()
-            for (const task of tasks){
+            for (const task of tasks) {
                 if (task.includes('!'))
                     console.log(task);
             }
             break;
         case command.startsWith('user'):
-            tasks = getAllTasks()
             let userName = command.split(' ')[1];
-            for (const task of tasks){
+            for (const task of tasks) {
                 if (task.toLowerCase().includes(userName.toLowerCase()))
                     console.log(task);
             }
+            break;
+        case command.startsWith('sort'):
+            arg = command.split(' ')[1];
+            if (arg === 'importance') {
+                let sortedTasks = [...tasks];
+                const countExclamations = (str) => str.split("!").length - 1;
+                sortedTasks.sort((a, b) => countExclamations(b) - countExclamations(a));
+                for (task of sortedTasks) {
+                    console.log(task);
+                }
+            }
+            if (arg === 'user') {
+                let sortedTasks = {};
+                for (const task of tasks) {
+                    if (task.includes(';')) {
+                        let user = task.split(';')[0].slice(8).toLowerCase();
+                        let comment = task.split(';')[2];
+                        if (user in sortedTasks)
+                            sortedTasks[user].push(comment);
+                        else {
+                            sortedTasks[user] = [comment];
+                        }
+                    }
+                }
+                for (const user of Object.keys(sortedTasks)){
+                    for (const comment of sortedTasks[user])
+                        console.log(`${user}: ${comment}`);
+                }
+            }
+
             break;
         default:
             console.log('wrong command');
